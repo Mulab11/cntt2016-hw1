6d38643879e18be01268e34549e8142e6826e156
zsyzzsoft <zsy19981012@126.com>
2016-10-21 15:12:05 +0800

    Update TC-SRM-557-div1-1000/XorAndSum.cpp

---
+++ TC-SRM-557-div1-1000/XorAndSum.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+using namespace std;
+class XorAndSum
+{
+public:
+	int n;
+	long long maxSum(vector<long long> a)
+	{
+		n = a.size();
+		int i = 0, j;
+		long long ans = 0;
+		for(long long t = 1LL << 62; t; t >>= 1) //高斯消元
+		{
+			for(j = i; j < n && !(j & t); j++);
+			if(j == n)
+				continue;
+			swap(a[i], a[j]);
+			for(j = 0; j < n; j++)
+			{
+				if(j != i && a[j] & t)
+					a[j] ^= a[i];
+			}
+			i++;
+		}
+		for(i = 1; i < n; i++) //计算答案
+			a[0] ^= a[i];
+		for(i = 1; i < n; i++)
+			ans += a[i] ^= a[0];
+		return ans;
+	}
+};
