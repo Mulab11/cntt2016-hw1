f4b3d8b6df91f5ac596c5f51d2b2ab96bcd4af4a
131131yhx <1876289824@qq.com>
2016-10-22 20:47:08 +0800

    Create 131131yhx.cpp

---
+++ TC-SRM-560-div1-250/131131yhx.cpp
@@ -0,0 +1,26 @@
+```C++
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int tot[51];
+
+class TomekPhone
+{
+	public: int minKeystrokes(vector <int> a, vector <int> b)
+	{
+		int ans = 0, sum = 0;
+		sort(a.begin(), a.end());
+		int n = a.size(), m = b.size();
+		for(int i = 0; i < m; i++) {
+			for(int j = 1; j <= b[i]; j++) tot[j]++;
+			sum += b[i];
+		}
+		if(sum < n) return -1;//check vaild
+		for(int i = n - 1; i >= 0; i--)
+			for(int j = 1; ; j++)
+				if(tot[j]) {tot[j]--, ans += j * a[i]; break;}//choose in increase order
+		return ans;
+	}
+};
+```
