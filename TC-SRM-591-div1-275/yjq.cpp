e73fa2823feefadf2737fd4c3110121b91defa73
yjqqqaq <309459245@qq.com>
2016-11-07 17:29:01 +0800

    Update TC-SRM-591-div1-275/yjq.cpp

---
+++ TC-SRM-591-div1-275/yjq.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+const int MAXN = 100010 ; 
+
+class TheTree {
+    public:
+		int n, cnt[MAXN] ; 
+		int maximumDiameter(vector<int> a) {
+				n = a.size() ; 
+				for (int i = 1;i <= n;i ++) cnt[i] = a[i - 1] ; 
+				cnt[0] = 1;
+			    int last = n + 1, ans = 0 ; 	
+                // 用last维护当前的b， ans表示答案
+				for (int i = n ;i >= 0 ;i --) {
+						ans = max(ans, n - i + last - 1 - i) ;
+					    if (cnt[i] == 1) last = i ;  //更新last
+				}
+				return ans ; 
+		}
+};
+			
+
+
+
+
+				
+
+
