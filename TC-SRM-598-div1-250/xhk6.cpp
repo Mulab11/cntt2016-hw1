fc781ca965c79cfc0045a53b4e0761c86a467c84
xhk6 <1945623890@qq.com>
2016-11-09 20:10:26 +0800

    Add files via upload

---
+++ TC-SRM-598-div1-250/xhk6.cpp
@@ -0,0 +1,60 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "BinPacking.cpp"
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,ans;
+int a[110],used[110];
+
+class BinPacking
+{	public:
+	int minBins(vector <int> item)
+	{	int i,j,s;
+	
+	 	ans=0;
+		n=item.size();
+		for(i=1;i<=n;i++) a[i]=item[i-1];
+		sort(a+1,a+n+1); //按升序排列 
+		memset(used,0,sizeof(used));
+		for(i=n;i>=1;i--) 
+		{	if(used[i]) continue;
+			if(a[i]==100) break; //最后剩下重量为100的单独处理 
+			for(j=i-1;j>=1;j--) //为每个i找到一个最大的j使得item[i]+item[j]<=300 
+				if(!used[j] && a[i]+a[j]<=300)
+				{	used[j]=1;
+					break;
+				}
+			used[i]=1;
+			ans++;
+		}
+		s=0;
+		for(i=1;i<=n;i++) s+=1-used[i];
+		if(s) ans+=(s-1)/3+1; //重量为100的每3个放一个桶 
+		return ans;
+	}
+	
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {150, 150, 150, 150, 150}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; verify_case(0, Arg1, minBins(Arg0)); }
+	void test_case_1() { int Arr0[] = {130, 140, 150, 160}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; verify_case(1, Arg1, minBins(Arg0)); }
+	void test_case_2() { int Arr0[] = {100, 100, 100, 100, 100, 100, 100, 100, 100}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; verify_case(2, Arg1, minBins(Arg0)); }
+	void test_case_3() { int Arr0[] = {100, 200, 100, 100, 100, 100, 200, 100, 200}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; verify_case(3, Arg1, minBins(Arg0)); }
+	void test_case_4() { int Arr0[] = {157, 142, 167, 133, 135, 157, 143, 160, 141, 123, 162, 159, 165, 137, 138, 152}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 8; verify_case(4, Arg1, minBins(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{	BinPacking ___test;
+	___test.run_test(-1);
+	return 0;
+}
+// END CUT HERE
