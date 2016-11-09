b1c65d903866060c50a20359ae2d1330f6456ae1
hzt1 <hezhongtian1@126.com>
2016-11-09 14:18:12 +0800

    Add files via upload

---
+++ TC-SRM-592-div1-300/hzt.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#define Rep(i, x, y) for (int i = x; i <= y; i ++)
+#define Dwn(i, x, y) for (int i = x; i >= y; i --)
+#define RepE(i, x) for(int i = pos[x]; i; i = g[i].nex)
+using namespace std;
+typedef long long ll;
+typedef double db;
+int num[300];
+class LittleElephantAndBalls {
+public:
+	int getNumber(string s) {
+		int n = s.size(), ans = 0, k = 0;
+		Rep(i, 0, n - 1) {
+			ans += k;
+			num[ s[i] ] ++;
+			k += (num[ s[i] ] <= 2);
+		}
+		return ans;
+	}
+};
