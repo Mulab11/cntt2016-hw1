6de92dfd5ad52c52e420fb15232f4db8264f4af1
JOHNKRAM <xuehao14@126.com>
2016-11-18 10:55:51 +0800

    Update TC-SRM-548-div1-250/JOHNKRAM.cpp

--- TC-SRM-548-div1-250/JOHNKRAM.cpp
+++ TC-SRM-548-div1-250/JOHNKRAM.cpp
@@ -6,10 +6,10 @@ class KingdomAndTrees
         int minLevel(vector <int> heights)
         {
             int n=heights.size(),l=-1,r=1000000000,m,i,j;
-            while(l+1<r)//二分答案 
+            while(l+1<r)//浜屽垎绛旀 
             {
                 m=l+r>>1;
-                for(i=j=0;i<n;i++) if(j<heights[i])j=max(j+1,heights[i]-m);//判定 
+                for(i=j=0;i<n;i++) if(j<heights[i])j=max(j+1,heights[i]-m);//鍒ゅ畾 
                 else if(j+1>heights[i]+m)break;
                 else j++;
                 if(i==n)r=m;
