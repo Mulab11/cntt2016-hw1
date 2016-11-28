64fdc35df418d0536b8f9e51984329f1f93e0050
zsyzzsoft <ghost@gitbook.com>
2016-11-28 19:43:57 +0800

    Update TC-SRM-550-div1-300/zsyzzsoft.cpp

---
+++ TC-SRM-550-div1-300/zsyzzsoft.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+using namespace std;
+const int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+bool vis[2000][2000];
+class RotatingBot
+{
+public:
+	int l, r, b, t;
+	bool Step(int x, int y) //向前走一步，并更新边界 
+	{
+		if(vis[x][y])
+			return false;
+		l = min(l, x);
+		r = max(r, x);
+		b = min(b, y);
+		t = max(t, y);
+		return vis[x][y] = true;
+	}
+	bool Turn(int x, int y) //判断能否转向 
+	{
+		return vis[x][y] || x < l || x > r || y < b || y > t;
+	}
+	int minArea(vector<int> moves)
+	{
+		int x = 1000, y = 1000, dir = 3;
+		l = x, r = x, b = y, t = y;
+		vis[x][y] = true;
+		for(int i = 0; i < moves.size(); i++)
+		{
+			if(!Turn(x + dx[dir], y + dy[dir]))
+				return -1;
+			dir = (dir + 1) % 4; //转向 
+			for(int j = 0; j < moves[i]; j++) //模拟 
+			{
+				if(!Step(x += dx[dir], y += dy[dir]))
+					return -1;
+			}
+		}
+		return (r - l + 1) * (t - b + 1);
+	}
+};
