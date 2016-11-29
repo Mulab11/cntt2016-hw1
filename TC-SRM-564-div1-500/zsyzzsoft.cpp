9f2ead4631a05df09a6eab4eea8c84923aae5ab6
zsyzzsoft <ghost@gitbook.com>
2016-11-29 16:34:32 +0800

    Update TC-SRM-564-div1-500/zsyzzsoft.cpp

--- TC-SRM-564-div1-500/zsyzzsoft.cpp
+++ TC-SRM-564-div1-500/zsyzzsoft.cpp
@@ -11,10 +11,7 @@ public:
 	{
 		long long ans = 0;
 		if(k % 3 == 1) //三种颜色均未耗尽 
-		{
-			for(int x = k; x <= n; x++)
-				ans += Count(k, n - x - k);
-		}
+			ans += (long long)(n - k) * (n - k - 1) / 2;
 		for(int x = 0; x * 3 < k; x++) //一种颜色已耗尽 
 		{
 			if((k - x * 3) % 2 == 0)
