497c71e51844a3912f8b62c00f1240ef2e588e43
zsyzzsoft <ghost@gitbook.com>
2016-11-29 17:01:59 +0800

    Update TC-SRM-567-div1-250/zsyzzsoft.cpp

--- TC-SRM-567-div1-250/zsyzzsoft.cpp
+++ TC-SRM-567-div1-250/zsyzzsoft.cpp
@@ -1,40 +1,20 @@
-#include<algorithm>
+#include <algorithm>
+#include <math.h>
 using namespace std;
-int prime[77780], top, cnt;
-bool isp[77780];
-int Ans, t, s;
+bool flag[100000];
 class TheSquareRootDilemma
 {
 public:
 	int countPairs(int n, int m)
 	{
-		for(int i = 2; i <= n; i++)
+		int ans = 0;
+		for(int i = 2; i * i <= n; i++)
 		{
-			if(isp[i] == 0)
-			{
-				for(int j = 2 * i; j <= n; j += i)
-					isp[j] = 1;
-			}
+			for(int j = i * i; j <= n; j += i * i)
+				flag[j] = true;
 		}
-		for(int i = 2; i <= n; i++)
-		{
-			if(isp[i] == 0)
-				prime[++top] = i;
-		}
-		for(int i = 2; i <= n; i++)
-		{
-			s = 1;
-			t = i;
-			for(int j = 1; j <= top; j++)
-			{
-				cnt = 0;
-				while(t % prime[j] == 0)
-					t /= prime[j], cnt++;
-				if(cnt % 2 == 1)
-					s *= prime[j];
-			}
-			Ans += (int)(sqrt((m / s)));
-		}
-		return Ans + (int)sqrt(m);
+		for(int i = 1; i <= n && i <= m; i++) //枚举公共约数 
+			ans += !flag[i] * (int)sqrt(n / i) * (int)sqrt(m / i);
+		return ans;
 	}
 };
