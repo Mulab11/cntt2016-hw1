f78b81184ded324399ec7043f51bc2408a7f3822
zsyzzsoft <ghost@gitbook.com>
2016-11-29 15:28:50 +0800

    Update TC-SRM-557-div1-250/zsyzzsoft.cpp

--- TC-SRM-557-div1-250/zsyzzsoft.cpp
+++ TC-SRM-557-div1-250/zsyzzsoft.cpp
@@ -5,9 +5,11 @@ class FoxAndMountainEasy
 public:
 	string possible(int n, int a, int b, string history)
 	{
-		for(int i = 0; i < history.length(); i++)
+		int h = 0;
+		for(int i = 0; i < history.length(); i++, h = min(h, a))
 			history[i] == 'U' ? a++ : a--;
-		n -= history.length();
+		a -= h; //最小初始高度 
+		n -= history.length() - h;
 		return abs(a - b) <= n && (a + n - b) % 2 == 0 ? "YES" : "NO";
 	}
 };
