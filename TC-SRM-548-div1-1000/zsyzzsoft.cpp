5efdb060adc3cb6523e79207fe4e19fc84b97d1a
zsyzzsoft <ghost@gitbook.com>
2016-11-29 11:35:59 +0800

    Update TC-SRM-548-div1-1000/zsyzzsoft.cpp

--- TC-SRM-548-div1-1000/zsyzzsoft.cpp
+++ TC-SRM-548-div1-1000/zsyzzsoft.cpp
@@ -1,19 +1,18 @@
 #include <algorithm>
 using namespace std;
 const int p = 1000000007;
-int f[51][51][51][3][2], c[2501][51];
+int f[51][51][3][2], c[2501][51];
 class KingdomAndCities
 {
 public:
-	int Work(int i, int j, int x, int y, int z)
+	int Work(int j, int x, int y, int z)
 	{
 		int n = x + y * 2 + z * 3;
 		if(n == 0)
 			return 0;
-		int ret = 0;
+		int ret = c[n * (n - 1) / 2][j];
 		for(int k = 0; k <= j; k++) //枚举子连通块内部边数 
 		{
-			int tmp = c[n * (n - 1) / 2][k];
 			int tx = x, ty = y, tz = z;
 			if(x)
 				tx--;
@@ -26,10 +25,13 @@ public:
 				for(int _y = 0; _y <= ty; _y++)
 				{
 					for(int _z = 0; _x + _y + _z < tx + ty + tz; _z++)
-						tmp = (tmp + p - (long long)f[n][k][_x + x - tx][_y + y - ty][_z + z - tz] * c[tx][_x] % p * c[ty][_y] % p * c[tz][_z] % p) % p;
+					{
+						int cx = _x + x - tx, cy = _y + y - ty, cz = _z + z - tz, cn = cx + cy * 2 + cz * 3;
+						ret = (ret + p - (long long)f[k][cx][cy][cz] * c[tx][_x] % p * c[ty][_y] % p * c[tz][_z] % p *
+							c[(n - cn) * (n - cn - 1) / 2][j - k] % p) % p; //连通块外任意连边 
+					}
 				}
 			}
-			ret = ((long long)tmp * c[(i - n) * (i - n - 1) / 2][j - k] + ret) % p; //连通块外任意取 
 		}
 		return ret;
 	}
@@ -37,7 +39,7 @@ public:
 	{
 		if(x < 0 || y < 0 || z < 0 || k < 0)
 			return 0;
-		return f[x + y * 2 + z * 3][k][x][y][z];
+		return f[k][x][y][z];
 	}
 	int howMany(int n, int m, int k)
 	{
@@ -52,17 +54,14 @@ public:
 			for(int j = 1; j < i && j <= 50; j++)
 				c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % p;
 		}
-		for(int i = 1; i <= n; i++) //总点数 
+		for(int j = 0; j <= k; j++) //总边数 
 		{
-			for(int j = 0; j <= k; j++) //总边数 
+			for(int x = 0; x <= n; x++) //连通块中包含单点的数量 
 			{
-				for(int x = 0; x <= i; x++) //连通块中包含单点的数量 
+				for(int y = 0; y <= 2 && x + y * 2 <= n; y++) //连通块中包含双点组的数量 
 				{
-					for(int y = 0; x + y * 2 <= i; y++) //连通块中包含双点组的数量 
-					{
-						for(int z = 0; x + y * 2 + z * 3 <= i; z++) //连通块中包含三点组的数量 
-							f[i][j][x][y][z] = Work(i, j, x, y, z);
-					}
+					for(int z = 0; z <= 1 && x + y * 2 + z * 3 <= n; z++) //连通块中包含三点组的数量 
+						f[j][x][y][z] = Work(j, x, y, z);
 				}
 			}
 		}
