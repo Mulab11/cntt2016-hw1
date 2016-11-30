a4a89d3e67f95958488baa33da63eae2c1c21903
zsyzzsoft <ghost@gitbook.com>
2016-11-30 10:13:12 +0800

    Update TC-SRM-572-div1-500/zsyzzsoft.cpp

--- TC-SRM-572-div1-500/zsyzzsoft.cpp
+++ TC-SRM-572-div1-500/zsyzzsoft.cpp
@@ -8,10 +8,12 @@ public:
 	vector<int> pos[10][10];
 	void Dfs(int depth)
 	{
+		int rest = 0;
 		for(int i = 0; i < m; i++) //不符合条件 
 		{
 			if(match[i] < 0 || match[i] > n - depth)
 				return;
+			rest += match[i];
 		}
 		if(depth == n)
 		{
@@ -24,14 +26,24 @@ public:
 			}
 			return;
 		}
-		for(int i = 0; i < 10; i++) //枚举 
+		for(int i = 0, flag = 0; i < 10; i++) //枚举 
 		{
+			if(flag && !pos[depth][i].size()) //都不匹配的不用枚举第二次 
+			{
+				if(flag == 2) //但要注意多组解的情况 
+					ans[0] = -1;
+				continue;
+			}
 			cur[depth] = i + '0';
 			for(auto it = pos[depth][i].begin(); it != pos[depth][i].end(); ++it)
 				match[*it]--;
+			if(!pos[depth][i].size())
+				flag = ans[0] == 0 ? 2 : 1;
 			Dfs(depth + 1);
 			for(auto it = pos[depth][i].begin(); it != pos[depth][i].end(); ++it)
 				match[*it]++;
+			if(!pos[depth][i].size() && flag == 2 && ans[0] == 0)
+				flag = 1;
 		}
 	}
 	string getNumber(vector<string> guesses, vector<int> bulls)
