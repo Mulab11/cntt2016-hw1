abca325cdb2926e9a0931cb2592f0fc45de7ffd3
Orz-zrO <1627711948@qq.com>
2016-12-08 20:41:38 +0800

    Add files via upload

---
+++ TC-SRM-587-div1-550/cjy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class TriangleXor
+{
+	public:
+		int theArea(int x)
+		{
+			double s=.5*x*(x+1);
+			for(int i=1;i<=x;i++)if(i&1)s-=1.*x*x/(x+i)*(x-i+1);else s+=1.*x*x/(x+i)*(x-i+1);//ÈÝ³â 
+			return floor(s);
+		}
+};
