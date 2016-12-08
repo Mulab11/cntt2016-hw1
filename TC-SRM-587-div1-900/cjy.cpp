edc14904996e40567b0c736d45d8c5b7b318f863
Orz-zrO <1627711948@qq.com>
2016-12-08 20:35:12 +0800

    Update cjy.cpp

--- TC-SRM-587-div1-900/cjy.cpp
+++ TC-SRM-587-div1-900/cjy.cpp
@@ -18,12 +18,12 @@ class ThreeColorability
 			memset(b,-1,sizeof(b));
 			memset(c,-1,sizeof(c));
 			for(j=0;j<a[0].size();j++)for(i=0;i<a.size();i++)for(k=i+1;k<a.size();k++)if(a[i][j]!='?'&&a[k][j]!='?')//连边			
-{
+			{
 				if(~b[i][k]&&b[i][k]!=(a[i][j]!=a[k][j]))return 0;
 				b[i][k]=b[k][i]=(a[i][j]!=a[k][j]);
 			}
 			for(i=0;i<a.size();i++)if(c[i]<0&&!dfs(i,0))return 0;//二分图染色			
-return 1;
+			return 1;
 		}
 		vector<string> lexSmallest(vector<string> A)
 		{
