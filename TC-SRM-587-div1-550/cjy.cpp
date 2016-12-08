54af0ea4968c29d8a4ad86c97574c9cd85105de4
Orz-zrO <1627711948@qq.com>
2016-12-08 20:42:09 +0800

    Update cjy.cpp

--- TC-SRM-587-div1-550/cjy.cpp
+++ TC-SRM-587-div1-550/cjy.cpp
@@ -6,7 +6,7 @@ class TriangleXor
 		int theArea(int x)
 		{
 			double s=.5*x*(x+1);
-			for(int i=1;i<=x;i++)if(i&1)s-=1.*x*x/(x+i)*(x-i+1);else s+=1.*x*x/(x+i)*(x-i+1);//ÈÝ³â 
+			for(int i=1;i<=x;i++)if(i&1)s-=1.*x*x/(x+i)*(x-i+1);else s+=1.*x*x/(x+i)*(x-i+1);//å®¹æ–¥			
 			return floor(s);
 		}
 };
