56072cbc7913d2d3a422c15582bbbcc3b46afe1e
Orz-zrO <1627711948@qq.com>
2016-12-11 17:32:53 +0800

    Add files via upload

---
+++ TC-SRM-591-div1-275/cjy.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,s,i,r;
+class TheTree
+{
+	public:
+		int maximumDiameter(vector<int>c)
+		{
+			n=c.size();
+			for(i=0;i<n;i++)
+			{
+				for(r=i;r<n&&c[r]>1;r++);
+				s=max(s,r+n-i-i);
+			}
+			return s;
+		}
+};
