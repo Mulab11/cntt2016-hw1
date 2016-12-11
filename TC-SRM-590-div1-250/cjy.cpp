b1dcff83f743884a8a59b8f65e9d49bd166c4146
Orz-zrO <1627711948@qq.com>
2016-12-11 17:26:44 +0800

    Add files via upload

---
+++ TC-SRM-590-div1-250/cjy.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define XQJ while(i<n-1&&a[i]=='.')i++;while(j<n-1&&b[j]=='.')j++;
+#define GG return"Impossible";
+using namespace std;
+int i,j,n;
+class FoxAndChess
+{
+	public:
+		string ableToMove(string a,string b)
+		{
+			n=a.size();
+			while(i<n&&j<n)
+			{
+				XQJ
+				if(a[i]!=b[j]||i!=j&&((a[i]=='L')^(i>j)))GG
+				i++;j++;
+			}
+			XQJ
+			if(max(a[i],b[j])>='L')GG
+			return"Possible";
+		}
+};
