d750aeb083c3d63f96c7bafd4fcd189417acb4ed
Orz-zrO <1627711948@qq.com>
2016-12-11 20:59:27 +0800

    Add files via upload

---
+++ TC-SRM-595-div1-900/cjy.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct P
+{
+	int x,y;
+	P operator -(const P &a)const{return P{x-a.x,y-a.y};}
+	int operator *(const P &a)const{return x*a.y-y*a.x;}
+}a[55],b;
+int i,j,k,n,t;
+double s,r;
+int C(int a,int b,int c){return a<=b&&b<=c||c<=b&&b<=a;}
+class Constellation
+{
+	public:
+		double expectation(vector<int>x,vector<int>y,vector<int>p)
+		{
+			n=x.size();
+			for(;i<n;i++)a[i]=P{x[i],y[i]};
+			for(i=0;i<n;i++)for(j=0;j<n;j++)if(i!=j)
+			{
+				r=1;b=a[j]-a[i];
+				for(k=0;k<n;k++)if(i!=k&&j!=k)if(t=b*(a[k]-a[i]),t>0||!t&&C(x[i],x[k],x[j])&&C(y[i],y[k],y[j]))r*=1-.001*p[k];
+				s+=r*(a[j]*a[i])*.000001*p[i]*p[j];
+			}
+			return s/2;
+		}
+};
