30f4acb57bd7aba97b58b780dc7781b9b68f05b1
Orz-zrO <1627711948@qq.com>
2016-12-11 17:33:33 +0800

    Update cjy.cpp

--- TC-SRM-591-div1-275/cjy.cpp
+++ TC-SRM-591-div1-275/cjy.cpp
@@ -7,7 +7,7 @@ class TheTree
 		int maximumDiameter(vector<int>c)
 		{
 			n=c.size();
-			for(i=0;i<n;i++)
+			for(i=0;i<n;i++)//枚举LCA			
 			{
 				for(r=i;r<n&&c[r]>1;r++);
 				s=max(s,r+n-i-i);
