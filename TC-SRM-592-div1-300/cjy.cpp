d67e04faf9457590f5d29668f62f732171373800
Orz-zrO <1627711948@qq.com>
2016-12-13 21:18:47 +0800

    Add files via upload

---
+++ TC-SRM-592-div1-300/cjy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int i,v[128],k,S;
+class LittleElephantAndBalls
+{
+	public:
+		int getNumber(string s)
+		{
+			for(i=0;i<s.size();i++)S+=k,v[s[i]]++,k+=(v[s[i]]<3);
+			return S; 
+		}
+};
