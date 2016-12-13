15f4083fefb97bfe5c65e906edec9809eb1dd67c
Orz-zrO <1627711948@qq.com>
2016-12-13 21:23:12 +0800

    Update cjy.cpp

--- TC-SRM-597-div1-250/cjy.cpp
+++ TC-SRM-597-div1-250/cjy.cpp
@@ -8,7 +8,7 @@ struct LittleElephantAndString
 		{
 			for(i=j=A.size()-1;~i;i--)x[A[i]]++,x[B[i]]--;
 			for(i=j;~i;i--)if(x[A[i]])return -1;
-			for(i=j;~i;i--)
+			for(i=j;~i;i--)//求最长后缀使其是另一个串的子序列			
 			{
 				while(~j&&A[j]!=B[i])j--;
 				if(j--<0)break;
