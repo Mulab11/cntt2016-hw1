502a6f8877818eca6927e6762b936b39417dc134
Orz-zrO <1627711948@qq.com>
2016-12-17 12:00:41 +0800

    Update cjy.cpp

--- TC-SRM-584-div1-250/cjy.cpp
+++ TC-SRM-584-div1-250/cjy.cpp
@@ -8,7 +8,7 @@ class Egalitarianism
 		{
 			n=a.size();
 			for(;i<n;i++)for(j=i+1;j<n;j++)f[i][j]=f[j][i]=(a[i][j]=='Y'?1:99);
-			for(k=0;k<n;k++)for(i=0;i<n;i++)for(j=0;j<n;j++)f[i][j]=min(f[i][j],f[i][k]+f[k][j]);
+			for(k=0;k<n;k++)for(i=0;i<n;i++)for(j=0;j<n;j++)f[i][j]=min(f[i][j],f[i][k]+f[k][j]);//Floyd
 			for(i=0;i<n;i++)for(j=0;j<n;j++)s=max(s,f[i][j]);
 			return s==99?-1:s*x;
 		}
