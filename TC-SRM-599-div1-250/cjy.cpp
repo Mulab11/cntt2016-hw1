3172b464b0a03499bd177c998c74e54346f09714
Orz-zrO <1627711948@qq.com>
2016-12-17 10:58:36 +0800

    Add files via upload

---
+++ TC-SRM-599-div1-250/cjy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t,r,g,i;
+class BigFatInteger
+{
+	public:
+		int minOperations(int A,int B)
+		{
+			for(i=2;i<=A;r=max(r,(int)ceil(log2(g*B))),g=0,i++)if(A%i==0)for(t++;A%i==0;A/=i)g++;
+			return t+r;
+		}
+};
