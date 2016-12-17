6985775ed0040abe3613c2e87289b6cc3d63a495
Orz-zrO <1627711948@qq.com>
2016-12-17 10:59:38 +0800

    Update cjy.cpp

--- TC-SRM-599-div1-250/cjy.cpp
+++ TC-SRM-599-div1-250/cjy.cpp
@@ -7,6 +7,6 @@ class BigFatInteger
 		int minOperations(int A,int B)
 		{
 			for(i=2;i<=A;r=max(r,(int)ceil(log2(g*B))),g=0,i++)if(A%i==0)for(t++;A%i==0;A/=i)g++;
-			return t+r;
+			return t+r;//素因子个数加倍增次数
 		}
 };
