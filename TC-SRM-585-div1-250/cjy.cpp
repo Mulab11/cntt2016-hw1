9543d7fd0ed53307e81c96babc330a955b9f73d0
Orz-zrO <1627711948@qq.com>
2016-12-17 22:32:24 +0800

    Update cjy.cpp

--- TC-SRM-585-div1-250/cjy.cpp
+++ TC-SRM-585-div1-250/cjy.cpp
@@ -4,7 +4,7 @@ int f[1000001],P=1e9+7,i=2;
 class TrafficCongestion
 {
 	public:
-		int theMinCars(int n)
+		int theMinCars(int n)//递推		
 		{
 			for(f[0]=f[1]=1;i<=n;i++)f[i]=(2ll*f[i-2]+f[i-1])%P;
 			return f[n];
