fe2ba6f5fec0928ca19f03e0f3a6c16cacfe95f2
Orz-zrO <1627711948@qq.com>
2016-12-17 11:55:28 +0800

    Add files via upload

---
+++ TC-SRM-596-div1-250/cjy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int i,x,s;
+class IncrementAndDoubling
+{
+	public:
+		int getMin(vector<int>a)
+		{
+			for(;i<a.size();i++)s+=__builtin_popcount(a[i]),x=max(x,a[i]);
+			return s+(x?(int)log2(x):0);
+		}
+};
