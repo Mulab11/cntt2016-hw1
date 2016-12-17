b21ffb2384a2d4fa087e5d266cf99a0af6acfc02
Orz-zrO <1627711948@qq.com>
2016-12-17 22:31:59 +0800

    Add files via upload

---
+++ TC-SRM-585-div1-250/cjy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int f[1000001],P=1e9+7,i=2;
+class TrafficCongestion
+{
+	public:
+		int theMinCars(int n)
+		{
+			for(f[0]=f[1]=1;i<=n;i++)f[i]=(2ll*f[i-2]+f[i-1])%P;
+			return f[n];
+		}
+};
