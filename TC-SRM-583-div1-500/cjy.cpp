a7162c22c1b0ffac2a06c6c2e9a7cbbf9d8ee31e
Orz-zrO <1627711948@qq.com>
2016-12-18 11:07:04 +0800

    Update cjy.cpp

--- TC-SRM-583-div1-500/cjy.cpp
+++ TC-SRM-583-div1-500/cjy.cpp
@@ -4,7 +4,7 @@ int i,r,v[55],x;
 class TurnOnLamps
 {
 	public:
-		int minimize(vector<int> a,string s,string p)
+		int minimize(vector<int> a,string s,string p)//贪心构造
 		{
 			for(i=a.size();i;i--)if(p[i-1]=='0')v[a[i-1]]^=v[i];else x=(s[i-1]=='0'),r+=(x!=v[i]),v[a[i-1]]^=x;
 			return r+1>>1;
