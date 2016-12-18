80218dc7a0d7abefe5534a7f694a35ece376fb46
Orz-zrO <1627711948@qq.com>
2016-12-18 19:28:23 +0800

    Add files via upload

---
+++ TC-SRM-582-div1-250/cjy.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<pair<int,long long> >e;
+int n,m,i,j;
+long long S,ans;
+class SpaceWarDiv1
+{
+	public:
+		long long minimalFatigue(vector<int> g,vector<int> s,vector<long long> c)
+		{
+			for(n=g.size(),m=s.size();i<m;i++)e.push_back(make_pair(s[i],c[i]));
+			sort(e.begin(),e.end());
+			sort(g.begin(),g.end());
+			for(i=m-1,j=n-1;~i;i--)
+			{
+				S+=e[i].second;
+				while(~j&&g[j]>=e[i].first)j--;
+				if(j==n-1)return -1;
+				ans=max(ans,(S+n-2-j)/(n-1-j));
+			}
+			return ans;
+		}
+};
