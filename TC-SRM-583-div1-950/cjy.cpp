4b37885b1d25d581244fead143af6964c82b0242
Orz-zrO <1627711948@qq.com>
2016-12-18 11:08:10 +0800

    Update cjy.cpp

--- TC-SRM-583-div1-950/cjy.cpp
+++ TC-SRM-583-div1-950/cjy.cpp
@@ -9,7 +9,7 @@ class RandomPaintingOnABoard
 		{
 			for(n=b.size(),m=b[0].size();i<min(n,m);i++)for(j=0;j<max(n,m);j++)a[i][j]=(n<m?b[i][j]:b[j][i])-48,S+=a[i][j],s[j]+=a[i][j];
 			if(n>m)swap(n,m);
-			for(v[i=0]=-1;i<(1<<n);i++)
+			for(v[i=0]=-1;i<(1<<n);i++)//枚举行容斥列
 			{
 				for(v[i]=i%2?-v[i/2]:v[i/2],j=0;j<m;j++)g[j]=s[j];
 				for(j=t=0;j<n;j++)if(i>>j&1)for(k=0;k<m;k++)g[k]-=a[j][k],t+=a[j][k];
