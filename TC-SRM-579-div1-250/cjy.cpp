08c7c68d8472215acafaec4b1dcdfb55e1e14d33
Orz-zrO <1627711948@qq.com>
2016-12-19 21:18:24 +0800

    Update cjy.cpp

--- TC-SRM-579-div1-250/cjy.cpp
+++ TC-SRM-579-div1-250/cjy.cpp
@@ -9,7 +9,7 @@ class UndoHistory
 			for(;i<a.size()&&i<b.size()&&a[i]==b[i];i++);
 			return i;
 		}
-		int minPresses(vector<string> e)
+		int minPresses(vector<string> e)//贪心
 		{
 			int s=e[0].size()+e.size(),j,i=1,k;
 			for(;i<e.size();s+=e[i].size()-k+2,i++)for(j=0,k=(LCP(e[i-1],e[i])==e[i-1].size()?e[i-1].size()+2:0);j<i;j++)k=max(k,LCP(e[i],e[j]));
