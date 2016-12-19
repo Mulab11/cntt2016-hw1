4f098e74584ec4aa606d476d053b7e52cc903b37
Orz-zrO <1627711948@qq.com>
2016-12-19 21:17:28 +0800

    Add files via upload

---
+++ TC-SRM-579-div1-250/cjy.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class UndoHistory
+{
+	public:
+		int LCP(string a,string b)
+		{
+			int i=0;
+			for(;i<a.size()&&i<b.size()&&a[i]==b[i];i++);
+			return i;
+		}
+		int minPresses(vector<string> e)
+		{
+			int s=e[0].size()+e.size(),j,i=1,k;
+			for(;i<e.size();s+=e[i].size()-k+2,i++)for(j=0,k=(LCP(e[i-1],e[i])==e[i-1].size()?e[i-1].size()+2:0);j<i;j++)k=max(k,LCP(e[i],e[j]));
+			return s;
+		}
+};
