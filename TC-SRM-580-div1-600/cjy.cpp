23493c7ff651e4bd0399b9ddbbbb203214c797c2
Orz-zrO <1627711948@qq.com>
2016-12-20 10:42:33 +0800

    Update cjy.cpp

--- TC-SRM-580-div1-600/cjy.cpp
+++ TC-SRM-580-div1-600/cjy.cpp
@@ -7,7 +7,7 @@ int i,s,j,k,r,n,R=2555;
 class ShoutterDiv1
 {
 	public:
-		int count(vs A,vs B,vs C,vs D,vs E,vs F,vs G,vs H)
+		int count(vs A,vs B,vs C,vs D,vs E,vs F,vs G,vs H)//贪心
 		{
 			for(;i<A.size();i++)I+=A[i],J+=B[i],K+=C[i],L+=D[i],M+=E[i],N+=F[i],O+=G[i],P+=H[i];
 			for(n=I.size(),i=0;i<n;i++)a[i]=make_pair((I[i]-48)*1000+(J[i]-48)*100+(K[i]-48)*10+L[i]-48,(M[i]-48)*1000+(N[i]-48)*100+(O[i]-48)*10+P[i]-48),R=min(R,a[i].second);
