146770625e88d449a3252be54fa961e403285ac9
Orz-zrO <1627711948@qq.com>
2016-12-22 10:34:19 +0800

    Update cjy.cpp

--- TC-SRM-575-div1-500/cjy.cpp
+++ TC-SRM-575-div1-500/cjy.cpp
@@ -8,7 +8,7 @@ class TheSwapsDivOne
 		double find(vector<string> S,int K)
 		{
 			for(auto x : S)for(auto y : x)a[++n]=y-48,s+=a[n];
-			for(;i<K;i++)t=t*(n-3)/(n-1);
+			for(;i<K;i++)t=t*(n-3)/(n-1);//留在原位的概率
 			for(i=n;i;i--)r+=(t*a[i]+(1-t)*s/n)*i*(n-i+1);
 			return r*2/n/(n+1);
 		}
