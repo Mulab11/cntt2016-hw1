2cd37d5250e7c25777a443f0a948bab6bf8218ed
Orz-zrO <1627711948@qq.com>
2016-12-23 22:34:05 +0800

    Update cjy.cpp

--- TC-SRM-573-div1-250/cjy.cpp
+++ TC-SRM-573-div1-250/cjy.cpp
@@ -6,7 +6,7 @@ class TeamContest
 		int worstRank(vector<int> a)
 		{
 			int b=max(max(a[0],a[1]),a[2])+min(min(a[0],a[1]),a[2])+1,l=3,r=a.size(),s=1;
-			for(sort(a.begin()+3,a.end());--r;s++,l+=2)
+			for(sort(a.begin()+3,a.end());--r;s++,l+=2)//贪心
 			{
 				while(l<r&&a[l]<b-a[r])l++;
 				if(l+2>r)return s;
