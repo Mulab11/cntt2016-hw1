5c65c9439d966a44aadf38dfe3dc54ed490add48
Orz-zrO <1627711948@qq.com>
2016-12-23 22:33:22 +0800

    Add files via upload

---
+++ TC-SRM-573-div1-250/cjy.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class TeamContest
+{
+	public:
+		int worstRank(vector<int> a)
+		{
+			int b=max(max(a[0],a[1]),a[2])+min(min(a[0],a[1]),a[2])+1,l=3,r=a.size(),s=1;
+			for(sort(a.begin()+3,a.end());--r;s++,l+=2)
+			{
+				while(l<r&&a[l]<b-a[r])l++;
+				if(l+2>r)return s;
+			}
+		}
+};
