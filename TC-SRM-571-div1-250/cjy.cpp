e1ae23972270370d8117f3eaed435c94fcd84387
Orz-zrO <1627711948@qq.com>
2016-12-25 15:29:37 +0800

    Update cjy.cpp

--- TC-SRM-571-div1-250/cjy.cpp
+++ TC-SRM-571-div1-250/cjy.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int k,n;
 vector<string> a;
 void dfs(string x){if(x.size()){if(k>=min(n,50)||stoll(x)>n)return;a.push_back(x+".mp3");k++;}for(char c=48;c<58;c++)if(c!=48||x.size())dfs(x+c);}
-class FoxAndMp3
+class FoxAndMp3//搜索
 {
 	public:
 		vector<string> playList(int N){n=N;dfs("");return a;}
