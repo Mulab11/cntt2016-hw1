70c86b1afa6a30cf1611fd3f493ebf521780a478
Orz-zrO <1627711948@qq.com>
2016-12-25 12:11:05 +0800

    Update cjy.cpp

--- TC-SRM-570-div1-900/cjy.cpp
+++ TC-SRM-570-div1-900/cjy.cpp
@@ -5,7 +5,7 @@ int h[N],e[N],g[N],l[N],w[N],t=1,n,m,T,s[N],S,V[N],d[N],O=1<<30,i,j,s1,s2,t1,t2,
 void A(int u,int v,int f,int c=0){e[++t]=v;g[t]=h[u];w[t]=f;l[t]=c;h[u]=t;e[++t]=u;g[t]=h[v];l[t]=-c;h[v]=t;}
 int W(int u,int f){if(u==T){s1-=f,S+=f*d[T-1];return f;}int i,p=f,t,q;V[u]=1;for(i=h[u];i;i=g[i])if(w[i]&&!V[e[i]])if(t=d[e[i]]+l[i]-d[u],!t){if(q=W(e[i],min(w[i],p)),w[i]-=q,w[i^1]+=q,p-=q,!p)return f;}else s[e[i]]=min(s[e[i]],t);return f-p;}
 int C(){int i,t=O;for(i=0;i<=T;i++)if(!V[i])t=min(s[i],t),s[i]=O;for(i=0;i<=T;i++)if(V[i])d[i]+=t;return t!=O;}
-class CurvyonRails
+class CurvyonRails//分层费用流
 {
 	public:
 		int getmin(vector<string> a)
