7044abe1cbec3a1bd5e08127ff224c0628c1e3dd
Orz-zrO <1627711948@qq.com>
2016-12-25 17:42:15 +0800

    Add files via upload

---
+++ TC-SRM-572-div1-250/cjy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int i,j,a[128],k,x,n,S;
+class NewArenaPassword
+{
+	public:
+		int minChange(string s,int K)
+		{
+			for(n=s.size();i<n-K;S+=x-k,i++)for(memset(a,x=k=0,sizeof(a)),j=i;j<n;j+=n-K)x++,k=max(k,++a[s[j]]);
+			return S;
+		}
+};
