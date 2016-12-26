3ff489202d6359f9dd09146c105179388f6a9f83
philipsweng <philipsweng@gmail.com>
2016-12-26 10:51:25 +0800

    Create wwt.cpp

---
+++ TC-SRM-548-div1-250/wwt.cpp
@@ -0,0 +1,36 @@
+/*
+	二分答案，贪心维护当前结尾最小可以是多少。
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class KingdomAndTrees
+{
+public:
+	bool test(vector<int> heights,int lim)
+	{
+		int lst = 0; //结尾大小
+		for(int i = 0,n = heights.size();i < n;i ++)
+		{
+			int cur = heights[i];
+			if (cur > lst) lst = max(lst + 1,cur - lim); else
+			{
+				if (cur + lim <= lst) return 0; //不合法
+				lst ++; 
+			}
+		}
+		return 1;
+	}
+
+	int minLevel(vector<int> heights)
+	{
+		int l = 0,r = int(1e9) + 5,tmp;
+		for(int mid;l <= r;)
+		{
+			mid = l + r >> 1;
+			if (test(heights,mid)) tmp = mid,r = mid - 1; else l = mid + 1;
+		}
+		return tmp;
+	}
+};
