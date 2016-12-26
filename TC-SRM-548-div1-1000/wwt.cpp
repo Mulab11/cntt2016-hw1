b673faae5e2ec06c868d888e28fc4cd4a08a54fd
philipsweng <philipsweng@gmail.com>
2016-12-26 10:42:43 +0800

    Update wwt.cpp

--- TC-SRM-548-div1-1000/wwt.cpp
+++ TC-SRM-548-div1-1000/wwt.cpp
@@ -1,36 +1,63 @@
 /*
-	二分答案，贪心维护当前结尾最小可以是多少。
+设f[n][c][sta]表示1所处的联通块大小为n，边数为c，sta表示点2是否在联通块中，g[sta][n][c]表示随便连的方案。经典容斥即可。
 */
 #include <bits/stdc++.h>
 
 using namespace std;
 
-class KingdomAndTrees
+const int maxn = 55,maxm = 2505,mo = int(1e9) + 7;
+
+class KingdomAndCities
 {
 public:
-	bool test(vector<int> heights,int lim)
+	int c[maxm][maxm],g[3][maxn][maxn],f[maxn][maxn][2];
+
+	int pow(int a,int b)
 	{
-		int lst = 0;
-		for(int i = 0,n = heights.size();i < n;i ++)
-		{
-			int cur = heights[i];
-			if (cur > lst) lst = max(lst + 1,cur - lim); else
-			{
-				if (cur + lim <= lst) return 0;
-				lst ++;
-			}
-		}
-		return 1;
+		int tmp = 1;
+		for(;b;b >>= 1,a = a * 1ll * a % mo)
+			if (b & 1) tmp = tmp * 1ll * a % mo;
+		return tmp;
 	}
 
-	int minLevel(vector<int> heights)
+	int howMany(int n,int m,int k_lim)
 	{
-		int l = 0,r = int(1e9) + 5,tmp;
-		for(int mid;l <= r;)
+		for(int i = 0;i <= 2500;i ++)
 		{
-			mid = l + r >> 1;
-			if (test(heights,mid)) tmp = mid,r = mid - 1; else l = mid + 1;
+			c[i][0] = 1;
+			for(int j = 1;j <= i;j ++) c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % mo;
 		}
-		return tmp;
+		for(int i = 0;i <= n;i ++)
+			for(int e = 0;e <= k_lim;e ++)
+			{
+				g[0][i][e] = c[c[i][2]][e];
+				if (i && e >= 2) g[1][i][e] = g[0][i - 1][e - 2] * 1ll * c[i - 1][2] % mo;
+				if (i > 1)
+				{ 
+					if (e >= 4) g[2][i][e] = g[0][i - 2][e - 4] * 1ll * pow(c[i - 2][2],2) % mo;
+					if (e >= 3) g[2][i][e] = (g[2][i][e] + g[0][i - 2][e - 3] * 1ll * pow(i - 2,2)) % mo;
+				}
+			}
+		for(int i = 1;i <= n;i ++)
+			for(int e = 0;e <= k_lim;e ++)
+				for(int sta = 0;sta <= bool(m > 1);sta ++)
+				{
+					int &curf = f[i][e][sta];
+					curf = g[bool(m) + sta][i][e];
+					for(int j = 1;j < i;j ++)
+						for(int k = 0;k <= e;k ++)
+							for(int psta = 0;psta <= sta;psta ++)
+							{
+								int coef = f[j][k][psta];
+								if (sta && !psta) coef = coef * 1ll * c[i - 2][j - 1] % mo;
+								if (sta && psta)
+								{
+									if (j > 1) coef = coef * 1ll * c[i - 2][j - 2] % mo; else coef = 0;
+								}
+								if (!sta && !psta) coef = coef * 1ll * c[i - 1][j - 1] % mo;
+								curf = (curf - coef * 1ll *  g[sta - psta][i - j][e - k] % mo + mo) % mo;
+							}
+				}
+		return f[n][k_lim][m == 2];
 	}
 };
