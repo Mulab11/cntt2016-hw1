d095ae80b1263070b5945a3ba63d1dcdcdad9350
Orz-zrO <1627711948@qq.com>
2016-12-27 09:05:58 +0800

    Add files via upload

---
+++ TC-SRM-569-div1-500/cjy.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int i,x,s;
+vector<int> b;
+class TheJediTest
+{
+	public:
+		int minimumSupervisors(vector<int> a,int k)
+		{
+			for(b=a;i<a.size();i++)if(s+=a[i]/k,x=a[i]%k)if(i+1<a.size())if(x<=b[i])a[i+1]+=x;else a[i+1]-=min(a[i+1],k-x),s++;else s++;
+			return s;
+		}
+};
