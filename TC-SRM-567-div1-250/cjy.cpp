2841aadf96086b711f62e60245dad5ae2615a7d8
Orz-zrO <1627711948@qq.com>
2016-12-27 22:58:47 +0800

    Add files via upload

---
+++ TC-SRM-567-div1-250/cjy.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int i,j,d[99999],s;
+class TheSquareRootDilemma
+{
+	public:
+		int countPairs(int n,int m)
+		{
+			for(i=2;i*i<=n;i++)for(j=i*i;j<=n;j+=i*i)d[j]=1;
+			for(i=1;i<=min(n,m);i++)s+=!d[i]*(int)sqrt(n/i)*(int)sqrt(m/i);
+			return s;
+		}
+};
