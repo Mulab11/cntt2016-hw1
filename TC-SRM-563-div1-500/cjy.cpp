29660006865650ca2eb8c6f44cc00ac946eab71b
Orz-zrO <1627711948@qq.com>
2016-12-28 22:19:50 +0800

    Add files via upload

---
+++ TC-SRM-563-div1-500/cjy.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int f[55],s,i,j,n;
+class SpellCards
+{
+	public:
+		int maxDamage(vector<int> v,vector<int> w)
+		{
+			for(n=v.size();i<n;i++)for(j=n-v[i];j>=0;j--)f[j+v[i]]=max(f[j+v[i]],f[j]+w[i]);
+			for(i=n;~i;i--)s=max(s,f[i]);
+			return s;
+		}
+};
