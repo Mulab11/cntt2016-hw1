72fe6625120cdcf976a3d047754e0f3450e5c760
Orz-zrO <1627711948@qq.com>
2017-01-03 22:55:02 +0800

    Add files via upload

---
+++ TC-SRM-560-div1-500/cjy.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[300][300],b[300][300],f[300][300],i,j,k;
+class DrawingPointsDivOne
+{
+	public:
+		int maxSteps(vector<int> x,vector<int> y)
+		{
+			for(i=0;i<x.size();i++)a[x[i]+75][y[i]+75]=b[x[i]+75][y[i]+75]=1;
+			for(k=1;k<150;k++)
+			{
+				for(i=299;i;i--)for(j=299;j;j--)a[i][j]|=a[i-1][j-1]|a[i-1][j]|a[i][j-1];
+				for(i=298;i;i--)for(j=298;j;j--)if((f[i][j]=a[i][j]*(min(f[i+1][j],min(f[i][j+1],f[i+1][j+1]))+1))>k&&!b[i][j])return k-1;
+			}
+			return -1;
+		}
+};
