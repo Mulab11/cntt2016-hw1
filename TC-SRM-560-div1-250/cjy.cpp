4e4f8efbaeb823e6fb95145e30038e2e30db6ba8
Orz-zrO <1627711948@qq.com>
2017-01-03 22:53:15 +0800

    Add files via upload

---
+++ TC-SRM-560-div1-250/cjy.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,t[55],i=1,s;
+class TomekPhone
+{
+	public:
+		int minKeystrokes(vector<int> a,vector<int> b)
+		{
+			for(auto x:b)while(x)t[x--]++;
+			for(n=a.size(),sort(a.begin(),a.end());i<55&&n;i++)while(t[i]--&&n)s+=a[--n]*i;
+			return n?-1:s;
+		}
+};
