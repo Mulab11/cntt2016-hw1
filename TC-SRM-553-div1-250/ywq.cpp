a9ce0c6a16cd9050f59516de9d06dfa23d2ce0c5
sansirowaltz <sansirowaltz@outlook.com>
2017-01-03 15:26:42 +0800

    Update ywq.cpp

--- TC-SRM-553-div1-250/ywq.cpp
+++ TC-SRM-553-div1-250/ywq.cpp
@@ -18,50 +18,46 @@ typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<pii> vpii;
 
-map<pair<int,ll>,ll> dp;
-int n,a[300010];
-bool nprime[1000010];
+ll a[55];
+bool b[55];
 
-ll dfs(int k,ll x)
+struct Suminator 
 {
-	if (k>n) return 1;
-	if (x<a[k]) return 1;
-	if (ll(a[k])*a[k]>x) //we can arrange at most one prime
-	{
-		int l=k,r=n;
-		while (l<=r)
-		{
-			int mid=(l+r)>>1;
-			if (a[mid]<=x) l=mid+1; else r=mid-1;
-		}
-		return l-k+1;
-	}
-	ll s=dfs(k+1,x);
-	x/=a[k];
-	while (x)
-	{
-		s+=dfs(k+1,x);
-		x=x/a[k]/a[k];
-	}
-	return s;
-}
-
-struct HolyNumbers 
-{
-    long long count(long long upTo, int maximalPrime)
+    int findMissing(vector <int> program, int wantedResult)
     {
-    	n=0;
-    	//Euler's Sieve
-    	for (int i=2;i<=maximalPrime;i++)
-    	{
-    		if (!nprime[i]) a[++n]=i;
-    		for (int j=1;j<=n;j++)
+    	int n=program.size(),top=0;
+        //program[i]=0
+    	for (int i=0;i<n;i++)
+    		if (program[i]<=0)
     		{
-    			if (i*a[j]>maximalPrime) break;
-    			nprime[i*a[j]]=1;
-    			if (i%a[j]==0) break;
+    			if (top>=2) {top--;a[top]+=a[top+1];}
     		}
+    		else
+    			a[++top]=program[i];
+    	if (a[top]==wantedResult) return 0;
+        //program[i]=x(x>0)
+    	top=0;
+    	for (int i=0;i<n;i++)
+    		if (program[i]==0)
+    		{
+    			if (top>=2)
+    			{
+    				top--;a[top]+=a[top+1];b[top]|=b[top+1];
+    			}
+    		}
+    		else
+    			if (program[i]==-1)
+    			{
+    				a[++top]=0;b[top]=1;
+    			}
+    			else
+    			{
+    				a[++top]=program[i];b[top]=0;
+    			}
+    	if (!b[top])
+    	{
+    		if (a[top]==wantedResult) return 1; else return -1;
     	}
-    	return dfs(1,upTo);
+    	if (a[top]<wantedResult) return wantedResult-a[top]; else return -1;
     }
 };
