ea18cd4c23f703a1f220f27ea3aef1fc9dbeacf6
Orz-zrO <1627711948@qq.com>
2017-01-04 17:30:21 +0800

    Add files via upload

---
+++ TC-SRM-557-div1-250/cjy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int l,i;
+class FoxAndMountainEasy
+{
+	public:
+		string possible(int n,int a,int b,string s)
+		{
+			for(;i<s.size();i++)l=min(l,s[i]=='U'?++a:--a);
+			return a-=l,n-=s.size()-l,abs(a-b)<=n&&!(a-b+n&1)?"YES":"NO";
+		}
+};
