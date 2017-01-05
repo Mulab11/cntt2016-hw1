4ce5e4b25cc8e2bfd17e6460c4d47c79ee6123ef
Orz-zrO <1627711948@qq.com>
2017-01-05 22:06:22 +0800

    Update cjy.cpp

--- TC-SRM-556-div1-250/cjy.cpp
+++ TC-SRM-556-div1-250/cjy.cpp
@@ -4,7 +4,7 @@ vector<int> a;
 vector<string> e;
 int s,v[55][1024],n;
 void D(int x,int y){v[x][y]=1;s=max(s,y);for(int i=0;i<n;i++)if(e[x][i]=='Y'&&!v[i][y^a[i]])D(i,y^a[i]);}
-class XorTravelingSalesman
+class XorTravelingSalesman//搜索
 {
 	public:
 		int maxProfit(vector<int> A,vector<string> E){return n=A.size(),a=A,e=E,D(0,a[0]),s;}
