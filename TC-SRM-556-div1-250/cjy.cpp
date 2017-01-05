4cea38f9c7cde02582adbe064a8b8307f276192b
Orz-zrO <1627711948@qq.com>
2017-01-05 22:02:48 +0800

    Add files via upload

---
+++ TC-SRM-556-div1-250/cjy.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> a;
+vector<string> e;
+int s,v[55][1024],n;
+void D(int x,int y){v[x][y]=1;s=max(s,y);for(int i=0;i<n;i++)if(e[x][i]=='Y'&&!v[i][y^a[i]])D(i,y^a[i]);}
+class XorTravelingSalesman
+{
+	public:
+		int maxProfit(vector<int> A,vector<string> E){return n=A.size(),a=A,e=E,D(0,a[0]),s;}
+};
