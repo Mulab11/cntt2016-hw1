58e09fce227d9bdc6e6aa3a00e680f64aafbd379
Orz-zrO <1627711948@qq.com>
2017-01-07 15:51:17 +0800

    Add files via upload

---
+++ TC-SRM-555-div1-255/cjy.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long x,t;
+int n,f[51],i,j;
+class CuttingBitString
+{
+	public:
+		int getmin(string s)
+		{
+			for(n=s.size(),i=n-1;~i;i--)for(x=0,f[i]=99,j=i;j<n;j++)
+			{
+				if(x=x*2+s[j]-48,!x)break;
+				for(t=x;t%5==0;t/=5);
+				if(t==1)f[i]=min(f[i],f[j+1]+1);
+			}
+			return f[0]<99?f[0]:-1;
+		}
+};
