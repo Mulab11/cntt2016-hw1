720ffbc6241a31f0c2c5fc76cdebca8b13883223
Orz-zrO <1627711948@qq.com>
2017-01-08 12:12:12 +0800

    Add files via upload

---
+++ TC-SRM-552-div1-900/cjy.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define LL long long
+using namespace std;
+LL f[1<<20],p[1<<20],t,i,j;
+class HolyNumbers
+{
+	public:
+		LL dfs(LL k,LL n)
+		{
+			if(k==t)return 1;
+			if(p[k]*p[k+1]>n)return upper_bound(p+k,p+t,n)-p-k+1;
+			LL s=dfs(k+1,n),i=p[k];
+			for(;i<=n;i*=p[k]*p[k])s+=dfs(k+1,n/i);
+			return s;
+		}
+		LL count(LL n,LL P)
+		{
+			for(i=2;i<=P;i++)if(!f[i])for(p[t++]=i,j=i;j<=P;j+=i)f[j]=1;
+			return dfs(0,n);
+		}
+};
