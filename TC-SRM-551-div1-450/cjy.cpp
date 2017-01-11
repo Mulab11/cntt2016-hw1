bec81e1960cffb97b7d42a645386f5c5f9e0b747
Orz-zrO <1627711948@qq.com>
2017-01-11 21:34:59 +0800

    Add files via upload

---
+++ TC-SRM-551-div1-450/cjy.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int d[50][50],n,i,j,k;
+class ColorfulWolves
+{
+	public:
+		int getmin(vector<string> s)
+		{
+			for(n=s.size(),memset(d,9,sizeof(d));i<n;i++)for(j=k=0;j<n;j++)if(s[i][j]=='Y')d[i][j]=k++;
+			for(k=0;k<n;k++)for(i=0;i<n;i++)for(j=0;j<n;j++)d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
+			return d[0][n-1]<9999?d[0][n-1]:-1;
+		}
+};
