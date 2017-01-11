c3c0542dc404ddf992f0c8d7feca5da2537faa5d
Orz-zrO <1627711948@qq.com>
2017-01-11 23:43:31 +0800

    Add files via upload

---
+++ TC-SRM-553-div1-250/cjy.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,i,t,f,O=1e9+1;
+long long x,m;
+class Suminator
+{
+    public:
+        int findMissing(vector<int> a,int M)
+        {
+        	for(m=M,n=a.size(),i=n-1;~i;i--){if(a[i]<1)t++;else if(t-->=0)x+=a[i];if(t<0)break;}
+        	if(x==m)return 0;
+        	for(t=0,i=n-1;~i;i--){if(!a[i])t++;else if(t-->=0)a[i]>0?m-=a[i]:f=1;if(t<0)break;}
+        	if(f)return m>0&&m<O?m:-1;
+        	return m?-1:1;
+        }
+};
