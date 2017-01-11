78c3b0b9632b06e9768b4b4c06bdc0e72dd3236c
zsyzzsoft <ghost@gitbook.com>
2017-01-11 09:55:24 +0800

    Updates TC-SRM-592-div1-300/zsyzzsoft.cpp
    Auto commit by GitBook Editor

--- TC-SRM-592-div1-300/zsyzzsoft.cpp
+++ TC-SRM-592-div1-300/zsyzzsoft.cpp
@@ -8,7 +8,7 @@ public:
 		int ans = 0;
 		for(int i = 0, r = 0, g = 0, b = 0; i < s.length(); i++)
 		{
-			ans += min(r, 2) + min(g, 2) + min(b, 2); //一定可以把前面出现次数为2的颜色都分在两侧 
+			ans += min(r, 2) + min(g, 2) + min(b, 2); //一定可以把前面出现过两次的颜色都分在两侧 
 			if(s[i] == 'R')
 				r++;
 			else if(s[i] == 'G')
