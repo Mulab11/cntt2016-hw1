ccf5770fd747d0744ab71dfbbf56f9dfe023e27d
zsyzzsoft <ghost@gitbook.com>
2017-01-11 09:00:09 +0800

    Updates TC-SRM-591-div1-900/zsyzzsoft.cpp
    Auto commit by GitBook Editor

--- TC-SRM-591-div1-900/zsyzzsoft.cpp
+++ TC-SRM-591-div1-900/zsyzzsoft.cpp
@@ -1,42 +1,74 @@
 #include <algorithm>
 using namespace std;
-class PyramidSequences
+const int p = 1000000009;
+long long f[8][8][1 << 8][1 << 8];
+class StringPath
 {
 public:
-	int Gcd(int a, int b)
+	int n, m;
+	bool Valid(int i, int j, int s)
 	{
-		return a ? Gcd(b % a, a) : b;
+		if(!i)
+			return s & 1;
+		if(!j)
+			return s >> (m - 1);
+		return (s & 1) || (s >> (m - 1));
 	}
-	long long distinctPairs(int n, int m)
+	int Y(int s)
 	{
-		if(n > m)
-			swap(n, m);
-		int k = Gcd(n - 1, m - 1) * 2; //同余方程有解，要求k能整除a+b或a-b，即a与b的模k的余数相同或互补 
-		int x = n / k, y = m / k;
-		n %= k, m %= k; //按每种余数的出现次数考虑 
+		return ((s << 1) + 1) & ((1 << m) - 1);
+	}
+	int N(int s)
+	{
+		return (s << 1) & ((1 << m) - 1);
+	}
+	int countBoards(int _n, int _m, string a, string b)
+	{
+		n = _n, m = _m;
+		if(a[0] != b[0])
+			return 0;
+		f[0][0][1][1] = 1;
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < m; j++) //逐格转移 
+			{
+				if(i == n - 1 && j == m - 1)
+					break;
+				int ti = i, tj = j + 1;
+				if(tj == m)
+					ti++, tj = 0;
+				for(int u = 0; u < 1 << m; u++)
+				{
+					for(int v = 0; v < 1 << m; v++) //到达轮廓线上的每个格子的状态 
+					{
+						f[i][j][u][v] %= p;
+						if(a[ti + tj] == b[ti + tj] && Valid(ti, tj, u) && Valid(ti, tj, v)) //与某个串匹配 
+							f[ti][tj][Y(u)][Y(v)] += f[i][j][u][v];
+						if(Valid(ti, tj, u) && (!Valid(ti, tj, v) || a[ti + tj] != b[ti + tj]))
+							f[ti][tj][Y(u)][N(v)] += f[i][j][u][v];
+						if(Valid(ti, tj, v) && (!Valid(ti, tj, u) || a[ti + tj] != b[ti + tj]))
+							f[ti][tj][N(u)][Y(v)] += f[i][j][u][v];
+						if(!Valid(ti, tj, v) && !Valid(ti, tj, u)) //不匹配
+							f[ti][tj][N(u)][N(v)] += f[i][j][u][v] * 26;
+						else if(Valid(ti, tj, v) ^ Valid(ti, tj, u))
+							f[ti][tj][N(u)][N(v)] += f[i][j][u][v] * 25;
+						else if(a[ti + tj] == b[ti + tj])
+							f[ti][tj][N(u)][N(v)] += f[i][j][u][v] * 25;
+						else
+							f[ti][tj][N(u)][N(v)] += f[i][j][u][v] * 24;
+					}
+				}
+			}
+		}
 		long long ans = 0;
-		if(n > m)
-			swap(n, m), swap(x, y);
-		ans += (long long)n * (x + 1) * (y + 1); //余数相同的对数 
-		ans += (long long)(m - n) * x * (y + 1);
-		ans += (long long)(k - m) * x * y;
-		ans -= (long long)(x + (n > k / 2)) * (y + (m > k / 2)); //减去既相同又互补的对数 
-		n = max(n - 1, 0);
-		m = k - m;
-		k--;
-		m = min(m, k);
-		if(n > m) //余数互补的对数 
+		for(int i = 0; i < 1 << m; i++) //计算答案 
 		{
-			ans += (long long)m * (x + 1) * y;
-			ans += (long long)(n - m) * (x + 1) * (y + 1);
-			ans += (long long)(k - n) * x * (y + 1);
+			for(int j = 0; j < 1 << m; j++)
+			{
+				if(i & 1 && j & 1)
+					ans += f[n - 1][m - 1][i][j];
+			}
 		}
-		else
-		{
-			ans += (long long)n * (x + 1) * y;
-			ans += (long long)(m - n) * x * y;
-			ans += (long long)(k - m) * x * (y + 1);
-		}
-		return ans;
+		return ans % p;
 	}
 };
