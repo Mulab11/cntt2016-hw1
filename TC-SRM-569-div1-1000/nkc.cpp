78f67c7d9609f1bc2930608c953359f4baf8a6ed
NationalNight <904095866@qq.com>
2017-01-18 10:35:49 +0800

    Add files via upload

--- TC-SRM-569-div1-1000/nkc.cpp
+++ TC-SRM-569-div1-1000/nkc.cpp
@@ -2,33 +2,77 @@
 #include<cstdio>
 #include<cstring>
 #include<algorithm>
-#include<vector>
 using namespace std;
-struct RobotHerb
+struct matrix
+{long long a[17][17];};
+int D;
+long long mod=1e9+9;
+matrix tmp,st;
+matrix operator*(const matrix &x,const matrix &y)//矩阵乘法 
 {
-	long long getdist(int T, vector <int> a)
+	memset(tmp.a,0,sizeof(tmp.a));
+	int i,j,k;
+	for(i=0;i<=D;i++)
+	for(j=0;j<=D;j++)
+	for(k=0;k<=D;k++)
+	(tmp.a[i][j]+=x.a[i][k]*y.a[k][j])%=mod;
+	return tmp;
+}
+matrix ksm(matrix d,int c)
+{
+	matrix ret=st;
+	while(c)
 	{
-		long long x=0,y=0,now=0;
+		if(c&1) ret=ret*d;
+		d=d*d;c/=2;
+	}
+	return ret;
+}
+matrix T[41];
+struct MegaFactorial
+{
+	int countTrailingZeros(int N, int K, int B)
+	{
+		D=K;
+		int fn=1,p;
+		//对于所有的base分别处理 
+		if(B==2) p=2;
+		if(B==3) p=3;
+		if(B==4) p=2,mod*=2,fn=2;
+		if(B==5) p=5;
+		if(B==6) p=3;
+		if(B==7) p=7;
+		if(B==8) p=2,mod*=3,fn=3;
+		if(B==9) p=3,mod*=2,fn=2;
+		if(B==10) p=5;
 		int i,j;
-		for(i=0;i<a.size();i++)
+		for(i=0;i<=D;i++)
+		st.a[i][i]=1;//初始化单位矩阵 
+		int TMP=p,cnt=0;
+		T[0].a[0][0]=1;
+		for(i=1;i<=D;i++)
+		for(j=i;j<=D;j++)
+		T[0].a[i][j]=1;
+		while(TMP<=N)
 		{
-			if(now==0) y+=a[i];
-			if(now==1) x+=a[i];
-			if(now==2) y-=a[i];
-			if(now==3) x-=a[i];
-			(now+=a[i])%=4;
-		}
-		if(now==0) return (abs(x)+abs(y))*T;//方向相同直接计算 
-		T%=4;//方向不同四次一定循环 
-		long long xx=0,yy=0,nnow=0;
-		for(i=1;i<=T;i++)
+			cnt++;
+			T[cnt]=ksm(T[cnt-1],p);
+			for(i=1;i<=D;i++)
+			T[cnt].a[0][i]++;
+			TMP*=p;
+		}//处理k次方项 
+		TMP/=p;
+		matrix di=st;
+		while(N!=0)
 		{
-			if(nnow==0) xx+=x,yy+=y;
-			if(nnow==1) yy-=x,xx+=y;
-			if(nnow==2) xx-=x,yy-=y;
-			if(nnow==3) xx-=y,yy+=x;
-			(nnow+=now)%=4;
+			if(TMP<=N)
+			{
+				while(TMP<=N)
+				di=di*T[cnt],N-=TMP;
+			}
+			cnt--;
+			TMP/=p;
 		}
-		return abs(xx)+abs(yy);
+		return di.a[0][D]/fn;
 	}
 };
