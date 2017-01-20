07704a3a2cd95e409110c84992f01648fb2307e1
Orz-zrO <1627711948@qq.com>
2017-01-20 16:23:09 +0800

    Update cjy.cpp

--- TC-SRM-593-div1-250/cjy.cpp
+++ TC-SRM-593-div1-250/cjy.cpp
@@ -1,19 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dx[6]={1,0,-1,-1,0,1},dy[6]={0,1,1,0,-1,-1},c[55][55],s=1,i,j,n;
+int dx[6]={1,0,-1,-1,0,1},dy[6]={0,1,1,0,-1,-1},c[55][55],s,i,j,n;
+vector<string> a;
 void dfs(int x,int y,int f)
 {
 	int i=0,u,v;
-	if(c[x][y])s=max(s,2*(c[x][y]!=f));
-	else for(;i<6;i++)if(u=x+dx[i],v=y+dy[i],~u&&~v&&u<n&&v<n)dfs(u,v,f^1);
+	if(c[x][y])s=max(s,3*(c[x][y]!=f));
+	else for(c[x][y]=f;i<6;i++)if(u=x+dx[i],v=y+dy[i],~u&&~v&&u<n&&v<n&&a[u][v]=='X')s=max(s,2),dfs(u,v,f^1);
 }
 class HexagonalBoard
 {
 	public:
-		int minColors(vector<string> a)
+		int minColors(vector<string> A)
 		{
-			n=a.size();
-			for(i=0;i<n;i++)for(j=0;j<n;j++)if(a[i][j]=='X'&&!c[i][j])dfs(i,j,2);
+			a=A;n=a.size();
+			for(i=0;i<n;i++)for(j=0;j<n;j++)if(a[i][j]=='X'&&!c[i][j])s=max(s,1),dfs(i,j,2);
 			return s;
 		}
 };
