701aac07dc6a530afd1f29af1d78af6025e06ad2
fateicee <244685407@qq.com>
2017-01-23 18:45:20 +0800

    Create fateice.cpp

---
+++ TC-SRM-599-div1-250/fateice.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<algorithm>
+#include<vector>
+#define L long long
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define inf 1000000000000000000ll
+#define eps 1e-9
+using namespace std;
+class BigFatIntegerCustom
+{
+	int p,q;
+public:
+	int minOperations(int a,int b)
+	{
+		int i,j;
+		for(i=2;i<=a;i++)
+		  if(a%i==0)
+		    {
+			 p++;
+			 j=0;
+			 while(a%i==0)
+			   a/=i,j++;
+			 q=max(q,j);
+			}
+		q*=b;
+		for(i=1,j=0;i<q;i*=2,j++);
+		return p+j;
+	}
+};
