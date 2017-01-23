58781f199297e55dc85d29fa61858589d060fcb4
xhk6 <1945623890@qq.com>
2017-01-23 23:15:19 +0800

    homework

---
+++ TC-SRM-566-div1-250/xhk.cpp
@@ -0,0 +1,78 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "PenguinSledding.cpp"
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+int d[110],ga[110][110];
+long long ans;
+
+class PenguinSledding
+{	public:
+	long long countDesigns(int numCheckpoints, vector <int> checkpoint1, vector <int> checkpoint2)
+	{	int i,j,k;
+		
+		n=numCheckpoints;
+		memset(d,0,sizeof(d));
+		memset(ga,0,sizeof(ga));
+		for(i=0;i<checkpoint1.size();i++)
+		{	ga[checkpoint1[i]][checkpoint2[i]]=1;
+			ga[checkpoint2[i]][checkpoint1[i]]=1;
+		}
+		ans=0;		
+		for(i=1;i<=n;i++)
+			for(j=i+1;j<=n;j++)
+			{	if(ga[i][j]) d[i]++,d[j]++; //统计度数 
+				for(k=j+1;k<=n;k++)
+					if(ga[i][j] && ga[j][k] && ga[i][k]) //三角形 
+						ans++;
+			}
+		for(i=1;i<=n;i++) //以i为中心的方案 
+			ans+=(1LL<<d[i])-1;
+		ans-=checkpoint1.size(); //去重 
+		return ans+1;		
+	}
+	
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arg0 = 2; int Arr1[] = {1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 2LL; verify_case(0, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+	void test_case_1() { int Arg0 = 4; int Arr1[] = {1,2,3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,3,4}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 6LL; verify_case(1, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+	void test_case_2() { int Arg0 = 6; int Arr1[] = {1,3,6}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,4,4}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 5LL; verify_case(2, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+	void test_case_3() { int Arg0 = 50; int Arr1[] = {40, 40, 40, 40, 40, 40, 40, 40, 
+ 40, 40, 40, 40, 50, 40, 40, 40, 
+ 23, 4, 24, 40, 22, 40, 8, 40, 40,
+ 40, 34, 21, 40, 40, 38, 40, 40, 
+ 40, 40, 40, 28, 40, 40, 40, 40, 
+ 46, 13, 40, 40, 40, 47, 40, 40}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {45, 42, 20, 48, 12, 32, 25, 10, 
+ 26, 39, 16, 2, 19, 43, 37, 17, 
+ 19, 19, 19, 18, 19, 27, 19, 29, 
+ 11, 36, 19, 19, 1, 41, 19, 35, 
+ 14, 33, 49, 3, 19, 7, 5, 19, 31, 
+ 19, 19, 6, 9, 15, 19, 44, 30}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 68719493118LL; verify_case(3, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+	void test_case_4() { int Arg0 = 36; int Arr1[] = {13, 24, 24, 20, 31, 16, 10, 36, 34, 32, 
+ 28, 5, 20, 29, 23, 2, 14, 4, 9, 5, 19, 
+ 21, 8, 1, 26, 27, 25, 15, 22, 30, 30, 
+ 6, 11, 7, 2, 35, 13, 29, 4, 12, 33, 18, 
+ 13, 14, 17, 35, 3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {10, 15, 27, 1, 29, 11, 5, 18, 33, 1, 9,
+ 2, 31, 6, 19, 10, 33, 18, 6, 27, 3, 22,
+ 4, 12, 31, 30, 34, 16, 7, 24, 3, 28, 15,
+ 21, 22, 8, 26, 20, 14, 32, 25, 17, 35,
+ 8, 36, 26, 23}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); long long Arg3 = 162LL; verify_case(4, Arg3, countDesigns(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{	PenguinSledding ___test;
+	___test.run_test(-1);
+	return 0;
+}
+// END CUT HERE
