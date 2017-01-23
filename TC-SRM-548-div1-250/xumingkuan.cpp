d488a8c3c9f94c85a7e3b5f357244a466ee839b3
xumingkuan <xumingkuan0721@126.com>
2017-01-23 14:20:26 +0800

    Update xumingkuan.cpp

--- TC-SRM-548-div1-250/xumingkuan.cpp
+++ TC-SRM-548-div1-250/xumingkuan.cpp
@@ -27,22 +27,22 @@ class KingdomAndTrees
 		}
 		return true;
 	}
-    public:
-    int minLevel(vector<int> heights)
+	public:
+	int minLevel(vector<int> heights)
 	{
-    	n = heights.size();
-    	for(int i = 0; i < n; i++)
-    		h[i] = heights[i];
-    	int l = 0, r = 1010101010;//er fen da an
-    	while(l < r)
-    	{
-    		int m = (l + r) >> 1;
-    		if(check(m))
-    			r = m;
-    		else
-    			l = m + 1;
-    	}
-        return l;
-    }
+		n = heights.size();
+		for(int i = 0; i < n; i++)
+			h[i] = heights[i];
+		int l = 0, r = 1010101010;//er fen da an
+		while(l < r)
+		{
+			int m = (l + r) >> 1;
+			if(check(m))
+				r = m;
+			else
+				l = m + 1;
+		}
+		return l;
+	}
 };
 
