606e344180bdc6a61944cfd848ca8419781619e8
fateicee <244685407@qq.com>
2017-01-23 21:05:35 +0800

    Update fateice.cpp

--- TC-SRM-569-div1-500/fateice.cpp
+++ TC-SRM-569-div1-500/fateice.cpp
@@ -9,84 +9,35 @@
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
 using namespace std;
-class EqualSums
+class TheJediTest
 {
-	int n,m,a[110],b[5010],c[5010],d[5010],f[110],p,q=1000000007;
-	bool x[110];
-	inline void add(int i,int j,int k)
-	{
-		b[++m]=j;
-		c[m]=k;
-		d[m]=a[i];
-		a[i]=m;
-	}
-	inline void dfs2(int i)
-	{
-		int j;
-		x[i]=1;
-		for(j=a[i];j>0;j=d[j])
-		  if(!x[b[j]])
-		    dfs2(b[j]);
-	}
-	inline void dfs(int i,int l)
-	{
-		int j;
-		if(i<n && f[i]<l || f[i]<0)
-		  {
-		   p=1;
-		   return;
-		  }
-		for(j=a[i];j>0;j=d[j])
-		  if(f[b[j]]!=-100 && f[b[j]]!=c[j]-f[i])
-		    {
-			 p=1;
-			 return;
-			}
-		  else if(f[b[j]]==-100)
-		    {
-			 f[b[j]]=c[j]-f[i];
-		     dfs(b[j],l);
-		     if(p)
-		       return;
-			}
-	}
-	inline int count(int l)
-	{
-		int i,j,k,u,ans=1;
-		for(i=0;i<n;i++)
-		  if(!x[i])
-		    {
-			 dfs2(i);
-			 u=0;
-		     for(j=l;j<10;j++)
-		       {
-			    p=0;
-			    for(k=0;k<2*n;k++)
-			      f[k]=-100;
-			    f[i]=j;
-			    dfs(i,l);
-			    if(!p)
-			      u++;
-			   }
-			 ans=(L)ans*u%q;
-			}
-		for(i=0;i<2*n;i++)
-		  x[i]=0;
-		return ans;
-	}
+	int n,y[60],p;
 public:
-	int count(vector<string> s)
+	int minimumSupervisors(vector<int> x,int m)
 	{
-		int i,j;
-		n=s.size();
+		int i;
+		n=x.size();
+		x.push_back(0);
+		//贪心 
 		for(i=0;i<n;i++)
-		  for(j=0;j<n;j++)
-		    if(s[i][j]!='-')
-		      {
-			   add(i,j+n,s[i][j]-'0');
-			   add(j+n,i,s[i][j]-'0');
-			  }
-		//容斥 
-		return (count(0)-count(1)+q)%q;
+		  {
+		   p+=x[i]/m;
+		   x[i]%=m;
+		   if(x[i+1]>=m-x[i]-y[i])
+		     {
+			  p++;
+			  x[i+1]-=m-x[i]-y[i];
+			 }
+		   else if(y[i])
+		     {
+		      p++;
+		      x[i+1]=0;
+			 }
+		   else
+		     y[i+1]=x[i];
+		  }
+		if(y[n])
+		  p++;
+		return p;
     }
 };
