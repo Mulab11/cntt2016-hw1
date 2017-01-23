2ee3b3d2aaba6c00b034c9d8bde30f242efa2351
fateicee <244685407@qq.com>
2017-01-23 21:05:48 +0800

    Update fateice.cpp

--- TC-SRM-569-div1-1000/fateice.cpp
+++ TC-SRM-569-div1-1000/fateice.cpp
@@ -9,118 +9,71 @@
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
 using namespace std;
-class DisjointSemicircles
+int n,m,p,r,pr[4]={2,3,5,7};
+long long u[20][20],v[20][20],w[20][20],t[20][20],q=1000000009;
+class MegaFactorial
 {
-	int n,m,a[110],b[5010],c[5010],d[5010],f[110],l[110],r[110],x[110],w[110],t,p,q;
-	bool g[110];
-	vector<int> s;
-	inline void add_(int i,int j,int k)
-	{
-		b[++m]=j;
-		c[m]=k;
-		d[m]=a[i];
-		a[i]=m;
-	}
-	inline void add(int i,int j,int k)
-	{
-		add_(i,j,k);
-		add_(j,i,k);
-	}
-	inline void dfs2(int i)
-	{
-		int j;
-	    g[i]=1;
-	    for(j=a[i];j>0;j=d[j])
-	      if(!g[b[j]])
-	        {
-			 f[b[j]]=f[i]^c[j];
-			 dfs2(b[j]);
-			}
-		  else if(f[b[j]]!=f[i]^c[j])
-		    q=1;
-	}
-	inline void pd()  //二分图染色 
-	{
-		int i,j,k,u;
-		for(i=1;i<=t;i++)
-		  {
-		   for(j=l[i],u=0;j<=r[i];j++)
-		     if(s[j-1]<0)
-		       u++;
-		   j=w[r[i]];
-		   if(j<l[i])
-		     continue;
-		   k=w[l[i]];
-		   if(x[i])
-		     add(j,k,0);
-		   else
-		     add(j,k,u&1);
-		  }
-		j=w[n];
-		add(0,j,0);
-		for(i=0;i<=n;i++)
-		  if(s[i-1]<0 && !g[i])
-		    {
-			 f[i]=0;
-			 dfs2(i);
-			}
-		if(!q)
-		  p=1;
-		q=0;
-		for(i=1;i<=m;i++)
-		  b[i]=c[i]=d[i]=0;
-		for(i=0;i<=n;i++)
-		  a[i]=g[i]=0;
-		m=0;
-	}
-	inline void dfs(int i)  //枚举每对已经匹配好的在上还是下 
-	{
-		if(i>t)
-		  {
-		   pd();
-		   return;
-		  }
-		if(l[i]==r[i]-1)
-		  {
-		   dfs(i+1);
-		   return;
-		  }
-		int j,k;
-		for(j=0;j<=1;j++)
-		  {
-		   x[i]=j;
-		   for(k=1;k<i;k++)
-		     if(x[i]==x[k] && (l[i]>l[k] && l[i]<r[k] && r[i]>r[k] || l[i]<l[k] && l[k]<r[i] && r[i]<r[k]))
-		       break;
-		   if(k==i)
-			 dfs(i+1);
-		  }
-	}
-public:
-	string getPossibility(vector<int> ss)
+	inline void cheng(long long a[20][20],long long b[20][20])  //矩乘 
 	{
 		int i,j,k;
-		s=ss;
-		n=s.size();
-		for(i=0;i<n/2;i++)
-		  for(j=0,k=0;j<n;j++)
-		    if(s[j]==i)
-		      if(!k)
-		        {
-				 k=1;
-				 l[++t]=j+1;
-				}
-			  else
-			    r[t]=j+1;
-		for(i=1;i<=n;i++)
+		for(i=0;i<=m;i++)
+		  for(j=i;j<=m;j++)
+			for(k=j;k<=m;k++)
+			  t[i][k]=(t[i][k]+a[i][j]*b[j][k])%q;
+		for(i=0;i<=m;i++)
+		  for(j=0;j<=m;j++)
+		    {
+			 b[i][j]=t[i][j];
+			 t[i][j]=0;
+			}
+	}
+public:
+	inline int countTrailingZeros(int nn,int mm,int b)
+	{
+		int i,j;
+		n=nn;
+		m=mm;
+		for(i=3;i>=0;i--)
+		  if(b%pr[i]==0)
+			{
+			 p=pr[i];
+			 while(b%p==0)
+			   {
+			    b/=p;
+			    r++;
+			   }
+			 break;
+			}
+		q*=r;
+		u[0][0]=1;
+		for(i=1;i<=m;i++)
+		  for(j=i;j<=m;j++)
+			u[i][j]=1;
+		for(i=0;i<=m;i++)
+		  w[i][i]=1;
+		//倍增 
+		for(;n>0;n/=p)
 		  {
-		   for(j=i;j>0 && s[j-1]>=0;j--);
-		   w[i]=j;
+		   for(i=0;i<=m;i++)
+		     v[i][i]=1;
+		   i=p,j=n%p;
+		   for(;i||j;i>>=1,j>>=1)
+		     {
+		      if(i&1)
+		        cheng(u,v);
+		      if(j&1)
+		        cheng(u,w);
+		      cheng(u,u);
+		     }
+		   for(i=0;i<=m;i++)
+		     for(j=0;j<=m;j++)
+		       {
+		        u[i][j]=v[i][j];
+		        v[i][j]=0;
+		   	   }
+		   for(i=1;i<=m;i++)
+		     u[0][i]=(u[0][i]+1)%q;
 		  }
-		dfs(1);
-		if(p)
-		  return "POSSIBLE";
-		else
-		  return "IMPOSSIBLE";
-    }
+		return (w[0][m]+q)%q/r;
+	}
 };
