39c347db50dc32a6eabb3061615a0e7e758189b7
j324h9 <sp399@qq.com>
2017-01-25 00:31:47 +0800

    Delete WrongAnswer.cpp

--- TC-SRM-554-Div1-250/WrongAnswer.cpp
+++
@@ -1,8 +0,0 @@
-class TheBrickTowerEasyDivOne{
-public:
-	int find(int A,int a,int B,int b){
-		int C=A<B?A:B;
-		if(a==b)return A==B?C*2:C*2+1; // 两种颜色高度相同
-		return A==B?3*C:3*C+1; // 两种颜色高度不同
-	}
-};
