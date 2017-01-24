c22cfc1148f2654ed1b29de7443b6fecbc6a452f
j324h9 <sp399@qq.com>
2017-01-25 00:31:29 +0800

    Delete WrongAnswer.cpp

--- TC-SRM-552-Div1-250/WrongAnswer.cpp
+++
@@ -1,8 +0,0 @@
-#define min(a,b) ((a)<(b)?(a):(b))
-class FoxPaintingBalls{
-public:
-	long long theMax(long long R, long long G, long long B, int N){
-		long long x=N*(N+1ll)/2,y=x/3;
-		return y?min(min(R/y,G/y),min(B/y,(R+G+B)/x)):R+G+B;
-	}
-};
