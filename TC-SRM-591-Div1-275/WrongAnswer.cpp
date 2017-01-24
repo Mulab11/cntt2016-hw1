3e38895006528a0932e9c1ee286acc0088dcfbfc
j324h9 <sp399@qq.com>
2017-01-25 01:02:38 +0800

    Delete WrongAnswer.cpp

--- TC-SRM-591-Div1-275/WrongAnswer.cpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-using namespace std;
-class TheTree{
-public:
-	int maximumDiameter(vector <int> cnt){
-		int D=cnt.size(),L=D,S=0;for(int i=D;i--;)D+(L=cnt[i]==1?i:L)-2*i>S?S=D+L-2*i:1;return S;
-	}
-};
